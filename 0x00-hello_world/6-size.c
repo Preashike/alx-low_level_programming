a1bae7dec02fa945b887d214484e4afbffefcebf
precious ikechukwu <ikechukwuogorchukwu8@gmail.com>
2023-07-13 18:09:49 -0700

    UNIX is basically a simple operating system

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,17 +5,10 @@
  */
 int main(void)
 {
- char a;
- int b;
- long int c;
- long long int d;
- float f;
-
 printf("Size of a char: %lu byte(s)\n", sizeof(char));	
-printf("size of a int: %lu byte(s)\n", sizeof(int));
+printf("size of an int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-printf("Size of a float: %1u byte(s)\n", sizeof(float)); 
-
+printf("Size of a float:%1u byte(s)\n", sizeof(float));
 return (0);
 }
